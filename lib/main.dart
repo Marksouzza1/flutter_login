d76f04b3e47f341657be5d50f6c1ad56e7592e15
Marksouzza1 <marcogs713@gmail.com>
2024-05-07 00:12:39 -0300

    inicial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_login/pages/login_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget{
+
+@override
+Widget build(BuildContext context) {
+  return MaterialApp(
+    title: 'login page',
+    theme: ThemeData(
+      primarySwatch: Colors.deepOrange,
+    ),
+    home: LoginPage(),
+  );
+}
+}
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(
+          top: 60,
+          left: 40,
+          right: 40,
+        ),
+        color: Colors.white,
+        child: ListView(
+          children: <Widget>[
+            SizedBox(
+              width: 128,
+              height: 128,
+              child: Image.asset('assetes/image.png'),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                labelText: 'E-mail',
+                labelStyle: TextStyle(
+                  color: Colors.black38,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 20,
+                )
+              ),
+            ),
+            SizedBox(
+              height:20,
+            ),
+              TextFormField(
+              keyboardType: TextInputType.text,
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Senha',
+                labelStyle: TextStyle(
+                  color: Colors.black38,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+                Container(
+                  height: 40,
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    style: TextButton.styleFrom(),
+                    onPressed: (){} ,
+                    child: Text('esqueci minha senha'),
+                  ),
+                ),
+          ],
+        ),
+      ),
+    );
+  }
+}
